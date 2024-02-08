96a72172bb6d0d09661d0e929534409b070912b9
Stilyan Dimitrov <sti4o_21@aol.com>
2024-02-08 03:30:49 -0600

    added an option to stop with q

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,19 @@
-number = 10 
- 
-print("I'm thinking of a number...") 
-guess = int(input("What number am I thinking of? ")) 
- 
-if guess == number: 
-   print("Congratulations! You guessed the right number.") 
-else: 
-   print(f"Sorry! The number was {number}.") 
+number = 10
+
+print("I'm thinking of a number...")
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit): ")
+    
+    if guess.lower() == 'q':
+        print(f"You've chosen to quit. The number was {number}.")
+        break
+    
+    try:
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break  # Exit the loop if the guess is correct
+        else:
+            print("Incorrect. Try again.")
+    except ValueError:
+        print("Please enter a valid number or 'q' to quit.")
